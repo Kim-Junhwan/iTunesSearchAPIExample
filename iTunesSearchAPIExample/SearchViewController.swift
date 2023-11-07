54a2de1872e902f76055acc844e47c0b76fa69c3
Kim junhwan <tazanboy1@naver.com>
2023-11-08 00:37:20 +0900

    프로젝트 생성

---
+++ iTunesSearchAPIExample/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  iTunesSearchAPIExample
+//
+//  Created by JunHwan Kim on 2023/11/08.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+}
